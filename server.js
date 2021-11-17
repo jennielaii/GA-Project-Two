e4e9899399f0bfeca9dab7dd900b74529322c0c9
jennielai <jennie.t.lai001@gmail.com>
2021-11-17 16:43:20 -0500

    wip:added middleware/depends

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express();
+const expressLayouts = require ('express-ejs-layouts');
 const PORT = process.env.PORT || 3000
 const rowdy = require('rowdy-logger');
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-    routesReport.print()
- })
 const routesReport = rowdy.begin(app)
-
 const userRoutes = require('./routes/userRoutes')
 // const listItemRoutes = require('./routes/listitemRoutes')
 
 app.use(express.json())
-app.use('/user', userRoutes)
+
+// set default view engine
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+// mount middleware 
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
 // app.use('/listItem', listItemRoutes)
+app.use('/user', userRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+    routesReport.print()
+ })
