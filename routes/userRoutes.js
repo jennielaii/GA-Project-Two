6d78e941c475310ec4d4895f3c72f9cc113151b4
jennielai <jennie.t.lai001@gmail.com>
2021-11-17 22:03:34 -0500

    wip

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,15 +4,13 @@ const userController = require('../controllers/userController');
 
 
 userRoutes.post('/register', userController.registerUser);
-userRoutes.post('/user/register', userController.showRegisterUser);
-userRoutes.post('/user/login', userController.showLoginUser);
+// userRoutes.post('/user/register', userController.showRegisterUser);
+// userRoutes.post('/user/login', userController.showLoginUser);
 userRoutes.post('/login', userController.loginUser);
 userRoutes.get('/logout', userController.logoutUser);
 userRoutes.post('/:id/checklist/new', userController.addUserChecklist);
 userRoutes.put('/:userId/checklist/edit/itemId', userController.editItem);
 userRoutes.delete('/checklist/delete/:id', userController.deleteItem);
 userRoutes.get('/:id/profile', userController.viewProfile);
-
-
 
 module.exports = userRoutes
